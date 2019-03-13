aefd0501c46b1e74d88bbb5101555ca956f8f3a4
Evgenii Ryshkov <eryshkov@gmail.com>
2019-03-13 11:21:36 +0300

    Iterator is implemented

--- App/View.php
+++ App/View.php
@@ -8,7 +8,7 @@ use App\Models\Article;
  * Class View
  * @property Article $articles
  */
-class View implements \Countable
+class View implements \Countable, \Iterator
 {
     protected $data = [];
 
@@ -44,5 +44,61 @@ class View implements \Countable
     public function count(): int
     {
         return count($this->data);
+    }
+
+    /**
+     * Return the current element
+     * @link https://php.net/manual/en/iterator.current.php
+     * @return mixed Can return any type.
+     * @since 5.0.0
+     */
+    public function current()
+    {
+        return current($this->data);
+    }
+
+    /**
+     * Move forward to next element
+     * @link https://php.net/manual/en/iterator.next.php
+     * @return void Any returned value is ignored.
+     * @since 5.0.0
+     */
+    public function next(): void
+    {
+        next($this->data);
+    }
+
+    /**
+     * Return the key of the current element
+     * @link https://php.net/manual/en/iterator.key.php
+     * @return mixed scalar on success, or null on failure.
+     * @since 5.0.0
+     */
+    public function key()
+    {
+        return key($this->data);
+    }
+
+    /**
+     * Checks if current position is valid
+     * @link https://php.net/manual/en/iterator.valid.php
+     * @return boolean The return value will be casted to boolean and then evaluated.
+     * Returns true on success or false on failure.
+     * @since 5.0.0
+     */
+    public function valid(): bool
+    {
+        return null !== key($this->data);
+    }
+
+    /**
+     * Rewind the Iterator to the first element
+     * @link https://php.net/manual/en/iterator.rewind.php
+     * @return void Any returned value is ignored.
+     * @since 5.0.0
+     */
+    public function rewind(): void
+    {
+        reset($this->data);
     }
 }
