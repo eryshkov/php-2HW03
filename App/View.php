523cf83d9f11ca763233be55613cc1ebf4df7350
Evgenii Ryshkov <eryshkov@gmail.com>
2019-03-15 09:08:14 +0300

    trait is added

--- App/View.php
+++ App/View.php
@@ -14,20 +14,7 @@ class View implements \Countable, \Iterator
 {
     protected $data = [];
 
-    public function __set($name, $value): void
-    {
-        $this->data[$name] = $value;
-    }
-
-    public function __get($name)
-    {
-        return $this->data[$name] ?? null;
-    }
-
-    public function __isset($name): bool
-    {
-        return isset($this->data[$name]);
-    }
+    use Magical;
 
     public function display(string $template): void
     {
